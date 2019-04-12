5729f16075c9e018055cfd4b9db5e2bb557332ba
genma2612 <manuelagustingonzalez@gmail.com>
2019-04-12 01:11:34 -0300

    Completo tabla html de empleado

--- Clase 4/Clase_04/admin.php
+++ Clase 4/Clase_04/admin.php
@@ -9,8 +9,36 @@ var_dump($_FILES);
 */
 
 $destino = "fotos_empleados/" . $_REQUEST["legajo"]."_".$_REQUEST["apellido"].".".pathinfo($_FILES["foto"]["name"], PATHINFO_EXTENSION);
-$E1 = new Empleado($_REQUEST["nombre"], $_REQUEST["apellido"], $_REQUEST["legajo"], $_REQUEST["sueldo"], $destino);
+$E1 = new Empleado($_REQUEST["legajo"], $_REQUEST["apellido"], $_REQUEST["nombre"], $_REQUEST["sueldo"], $destino);
 Empleado::Agregar($E1);
-move_uploaded_file(_$destino)
+if(move_uploaded_file($_FILES["foto"]["tmp_name"],$destino))
+{
+    echo "Se guardó correctamente.";
+}
 echo $E1->ToString()."<br>";
 var_dump(Empleado::TraerTodos());
+$empleados = Empleado::TraerTodos();
+echo "<br><br>";
+foreach($empleados as $emp)
+{
+echo 	'<table border="1" style="width:0%;height:0%">
+		<tr> 
+			<td >Nombre:</td>
+            <td>' . $emp->nombre . '</td>
+			<td rowspan="4"><img width=100 height=100 src=' .  $emp->path_foto . '/></td>
+        </tr>
+        <tr>
+            <td>Apellido:</td>
+			<td>' . $emp->apellido . '</td>
+        </tr>
+        <tr>
+            <td>Legajo:</td>
+            <td>' . $emp->legajo . '</td>
+        </tr>
+        <tr>
+            <td>Sueldo:</td>
+            <td>' . $emp->sueldo . '</td>
+        </tr>
+ 	</table><br>';
+}
+echo '<a href="http://localhost/Clase_04/index_02.html">Volver</a>';
