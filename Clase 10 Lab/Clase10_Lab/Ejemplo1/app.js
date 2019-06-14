1dab42c7d25688dbd82bf4c888fd761b8778251b
genma2612 <manuelagustingonzalez@gmail.com>
2019-06-14 11:45:51 -0300

    Final Clase 10 Lab

---
+++ Clase 10 Lab/Clase10_Lab/Ejemplo1/app.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $('#idForm').bootstrapValidator({
+        message: 'El valor no es vàlido',
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            usuario: {
+                validators: {
+                    notEmpty: {
+                        message: 'El nombre de usuario està vacio.'
+                    },
+                    stringLength: {
+                        min: 3,
+                        message: 'El minimo de caracteres admitido es de 3.'
+                    }
+                }
+            },
+            clave: {
+                validators: {
+                    notEmpty: {
+                        message: 'La contraseña està vacia.'
+                    },
+                    stringLength: {
+                        min: 4,
+                        max: 8,
+                        message: 'Ingrese entre 4 y 8 caracteres.'
+                    }
+                }
+            }
+        }
+    })
+});
